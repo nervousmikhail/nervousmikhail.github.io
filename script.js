6ecfbb5efced10e13c75981cdad5722584464d82
nervousmikhail <48534406+nervousmikhail@users.noreply.github.com>
2019-03-14 01:28:29 +0300

    Файлы для сайта

---
+++ script.js
@@ -0,0 +1,25 @@
+var prNum, tempOut;
+prNum = Math.floor((Math.random() * 10) + 1);
+//tempOut = document.getElementById('temp-out');
+//tempOut.innerHTML = prNum;
+
+function f1(){
+	var num, out;
+	
+	
+	
+	num = document.getElementById('mynum').value;
+	out = document.getElementById('out');
+	
+	if (num == prNum) {
+		out.innerHTML = 'Именно за столько минут Я сделал этот сайт';
+	}
+	else if (num >prNum){
+		out.innerHTML = 'Это слишком много';
+	}
+	else {
+		out.innerHTML = 'Это слишком мало';
+	}
+	
+	
+}
